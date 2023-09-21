3f2146d3746eb9e82d606ee6334b867e4ebad7c0
Amay Kevit <amay_lunagaria@kevit.io>
2023-09-21 12:55:16 +0530

    Before Purge

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("./oden/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("./varian/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["./oden/**/*.scss"], buildStyles);
+  watch(["./varian/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
